fedb68b7e0ac047b5260508745752bb5e96d1a78
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-06 17:58:12 +0500

    Add worker job and job execution history migrations

---
+++ src/WorkerScheduler.Persistence/Migrations/20240306124729_Add_WorkerJob.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkerScheduler.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_WorkerJob : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    JobType = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Status = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    CronSchedule = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false, defaultValue: "0 0 * * *")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_JobType",
+                table: "Jobs",
+                column: "JobType",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jobs");
+        }
+    }
+}
---
+++ src/WorkerScheduler.Persistence/Migrations/20240306125537_Add_WorkerJob_And_WorkerJobExecutionHistory_Relation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkerScheduler.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_WorkerJob_And_WorkerJobExecutionHistory_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobExecutionHistories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    JobId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsSuccessful = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobExecutionHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobExecutionHistories_Jobs_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Jobs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobExecutionHistories_JobId",
+                table: "JobExecutionHistories",
+                column: "JobId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobExecutionHistories");
+        }
+    }
+}
